6667315464e5f2302838298d25d5686c8bcfb7a0
knyazev_a_p <knyazev_artem@icloud.com>
2025-07-30 02:14:18 +0300

    add notebook with training model

--- backend/src/api/schemas.py
+++ backend/src/api/schemas.py
@@ -1,7 +1,33 @@
 from pydantic import BaseModel
+from typing import List, Dict
 
-class PredictRequest(BaseModel):
-    text: str
+class Entity(BaseModel):
+    word: str
+    entity_group: str
+    score: float
+    sentence: str
 
-class PredictResponse(BaseModel):
-    entities: list 
+class EntitiesResponse(BaseModel):
+    entities: List[Entity]
+
+class KGNode(BaseModel):
+    id: str
+    type: str
+    conf: float
+
+class KGEdge(BaseModel):
+    source: str
+    target: str
+    relation: str
+    conf: float
+
+class KGResponse(BaseModel):
+    nodes: List[KGNode]
+    links: List[KGEdge]
+
+class FileUploadResponse(BaseModel):
+    message: str
+    file_id: str
+
+class ErrorResponse(BaseModel):
+    detail: str
